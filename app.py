c13aece047c95635adde3235a28caddc7ff6534b
Jatin Kalra <142911658+jatinkalra1710@users.noreply.github.com>
2026-06-26 13:50:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ llm = HuggingFaceEndpoint(
 chat_model = ChatHuggingFace(llm=llm)
 
 embedding_model = HuggingFaceEmbeddings(
-    model_name="BAAI/bge-m3"
+    model_name="intfloat/multilingual-e5-small""
 )
 
 
